c9ea3dd3dd4ef63fc3122a659d3eeac76bdb83d9
Jeannot Diambu <diambujeannot@gmail.com>
2024-07-16 22:56:31 +0200

    remove page

--- lib/helpers/registerUser.dart
+++ lib/helpers/registerUser.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:login_signup/helpers/pages/home_page.dart';
+import 'package:login_signup/pages/home_page.dart';
 
 
 
